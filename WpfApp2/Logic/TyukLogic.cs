9975116576beede3048b2afb20b7dc2959dd2c1f
hallgato <hallgato@C202-20>
2024-03-22 12:26:14 +0100

    Add project files.

---
+++ WpfApp2/Logic/TyukLogic.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Models;
+
+namespace WpfApp2.Logic
+{
+    public class TyukLogic
+    {
+        IList<Tyuk> tyukok;
+        IMessenger messenger;
+
+        public TyukLogic(IMessenger messenger)
+        {
+            this.messenger = messenger;
+        }
+        public int TojasCount
+        {
+            get
+            {
+                return tyukok.Sum(t => t.Eggcount);
+            }
+        }
+        public void AddFood(string name)
+        {
+            foreach (var item in tyukok)
+            {
+                if (item.Name == name)
+                {
+                    item.Isfed = true;
+                }
+            }
+            messenger.Send("Food given", "Food");
+        }
+        public void RemoveEgg(string name)
+        {
+            int amount = 0;
+            foreach (var item in tyukok)
+            {
+                if (item.Name==name)
+                {
+                    amount += item.Eggcount;
+                    item.Eggcount = 0;
+                }
+            }
+            messenger.Send("Eggs removed", "Egg");
+        }
+    }
+}
